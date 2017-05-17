3d76f83c5d152aadbaa4c563f2103b8d37ee09a6
aipeng <781728378@qq.com>
2017-05-17 14:55:00 +0800

    redirect index

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -60,7 +60,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect(['small-story/index']);
+//        return $this->render('index');
     }
 
     /**
