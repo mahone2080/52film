8936df2c8880be1da8f6fc48f743645a4f93a44b
aipeng <781728378@qq.com>
2017-09-20 10:14:42 +0800

    optimized query field

--- frontend/views/film-studio/view.php
+++ frontend/views/film-studio/view.php
@@ -37,9 +37,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
     }
     echo '</div>';
-    if (Yii::$app->params['devicedetect']['isMobile']) {
+//    if (Yii::$app->params['devicedetect']['isMobile']) {
         echo '<div class="panel panel-warning"><div class="panel-heading">微信用户查看<mark>原网页</mark>或在<mark>电脑端</mark>打开可见<strong>下载地址</strong>^_^</div></div>';
-    }
+//    }
 
     echo '<div class="panel panel-success"><div class="panel-heading">';
     $arr_content = json_decode($model->content);
