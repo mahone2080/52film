0bdb22af33e1309792c4fee2670f2533f4e21729
aipeng <781728378@qq.com>
2019-01-03 11:25:22 +0800

    ad

--- frontend/views/film-studio/view.php
+++ frontend/views/film-studio/view.php
@@ -9,10 +9,31 @@ use yii\widgets\DetailView;
 $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'list', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerJs('
+    (function(win,doc){
+        var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
+        if (!win.alimamatk_show) {
+            s.charset = "gbk";
+            s.async = true;
+            s.src = "https://alimama.alicdn.com/tkapi.js";
+            h.insertBefore(s, h.firstChild);
+        };
+        var o = {
+            pid: "mm_46748187_297500171_81198500096",/*推广单元ID，用于区分不同的推广渠道*/
+            appkey: "25552029",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
+            unid: "52film-view",/*自定义统计字段*/
+            type: "click" /* click 组件的入口标志 （使用click组件必设）*/
+        };
+        win.alimamatk_onload = win.alimamatk_onload || [];
+        win.alimamatk_onload.push(o);
+    })(window,document);
+
+', ['position' => self::POS_END]);
+
 ?>
 <div class="film-studio-view">
 
-
+    <a data-type="13" data-tmpl="628x100" data-tmplid="213" data-rd="2" data-style="2" data-border="1" href="#"></a>
     <?php
     //    echo Html::img("data:image/jpeg;base64," . $model->thumb, ['alt' => 'thumb',]) . '<br>';
 
@@ -37,9 +58,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
     }
     echo '</div>';
-//    if (Yii::$app->params['devicedetect']['isMobile']) {
-//        echo '<div class="panel panel-warning"><div class="panel-heading">微信用户查看<mark>原网页</mark>或在<mark>电脑端</mark>打开可见<strong>下载地址</strong>^_^</div></div>';
-//    }
+    //    if (Yii::$app->params['devicedetect']['isMobile']) {
+    //        echo '<div class="panel panel-warning"><div class="panel-heading">微信用户查看<mark>原网页</mark>或在<mark>电脑端</mark>打开可见<strong>下载地址</strong>^_^</div></div>';
+    //    }
 
     echo '<div class="panel panel-success"><div class="panel-heading">';
     $arr_content = json_decode($model->content);
@@ -80,5 +101,4 @@ $this->params['breadcrumbs'][] = $this->title;
 
     }
     ?>
-
 </div>
