18a3d6e5a6bbdee30b8e7102e1ccbbe88b2c44f7
aipeng <781728378@qq.com>
2019-01-04 15:20:04 +0800

    ad debug

--- frontend/views/film-studio/view.php
+++ frontend/views/film-studio/view.php
@@ -33,7 +33,7 @@ $this->registerJs('
 ?>
 <div class="film-studio-view">
 
-    <a data-type="13" data-tmpl="628x100" data-tmplid="213" data-rd="2" data-style="2" data-border="1" href="https://s.click.taobao.com/t?e=m%3D2%26s%3DYnLV0HJaRcccQipKwQzePCperVdZeJviK7Vc7tFgwiFRAdhuF14FMWJmsTsIGnwhlovu%2FCElQOvMxElT2TXd%2Fell2FZesTAnrIMhMS0vef7RTwhgJXO8KU5U%2BEi82A4gU9jONtfgAYE735YeP4ao%2FPQho7GrRW4PnynZ4bvuOISqGxr42djOuvQ2dTYp%2Bn82sd%2B%2Ff4Fhw9acZaZLNbaIunA84LCy58V0xg5p7bh%2BFbQ%3D">
+    <a target="_blank" data-type="13" data-tmpl="628x100" data-tmplid="213" data-rd="2" data-style="2" data-border="1" href="https://s.click.taobao.com/t?e=m%3D2%26s%3DYnLV0HJaRcccQipKwQzePCperVdZeJviK7Vc7tFgwiFRAdhuF14FMWJmsTsIGnwhlovu%2FCElQOvMxElT2TXd%2Fell2FZesTAnrIMhMS0vef7RTwhgJXO8KU5U%2BEi82A4gU9jONtfgAYE735YeP4ao%2FPQho7GrRW4PnynZ4bvuOISqGxr42djOuvQ2dTYp%2Bn82sd%2B%2Ff4Fhw9acZaZLNbaIunA84LCy58V0xg5p7bh%2BFbQ%3D">
         <img src="https://gtms04.alicdn.com/tps/i4/TB1zBf8LFXXXXaAaXXXQO4D5VXX-440-180.jpg" />
     </a>
     <?php
