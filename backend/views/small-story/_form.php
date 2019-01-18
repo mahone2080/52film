0d5eec5ca4f92f83dd482852b825a1b3531f1b3d
aipeng <781728378@qq.com>
2019-01-18 13:10:50 +0800

    optimize

--- backend/views/small-story/_form.php
+++ backend/views/small-story/_form.php
@@ -27,7 +27,7 @@ $this->registerJs('')
     //echo $form->field($model, 'content')->widget(\kriss\wangEditor\WangEditorWidget::className());
     echo $form->field($model, 'content')->widget(\dosamigos\ckeditor\CKEditor::className(), [
 		'options' => ['rows' => 6],
-		'preset' => 'basic'
+//		'preset' => 'basic'
 	]); 
     ?>
     <?= $form->field($model, 'category')->dropDownList((Yii::$app->params['story'])) ?>
