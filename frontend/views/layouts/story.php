d9712603ba3d23ff12e349e528e4322ad80faebd
aipeng <781728378@qq.com>
2019-01-31 16:56:31 +0800

    optimize

--- frontend/views/layouts/story.php
+++ frontend/views/layouts/story.php
@@ -22,13 +22,13 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title . '-人生不是故事，人生是事故，摸爬滚打，才不辜负功名尘土。') ?></title>
     <?php $this->head() ?>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3144712571787670",
-    enable_page_level_ads: true
-  });
-</script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-3144712571787670",
+            enable_page_level_ads: true
+        });
+    </script>
     <?php
     $this->registerCssFile('@web/css/offcanvas.css');
     $this->registerCssFile('@web/css/_shadows.css');
@@ -69,13 +69,14 @@ AppAsset::register($this);
     ]);
     NavBar::end();
     $get = Yii::$app->request->get();
-    $keyword = isset($get['SmallStorySearch']['content'])?$get['SmallStorySearch']['content']:'';
-?>
+    $keyword = isset($get['SmallStorySearch']['content']) ? $get['SmallStorySearch']['content'] : '';
+    ?>
     <div class="nav-scroller bg-white shadow-xs hidden-md hidden-lg hidden-sm col-xs-12">
         <nav class="nav nav-underline">
             <?php
             echo Html::a('菜根文学', ['small-story/index', 'category' => 'qingsongzhai'], ['class' => 'nav-link']);
-//            echo Html::a('看电影', 'https://www.52film.top', ['class' => 'nav-link', 'target' => '_blank']);
+            echo Html::a('金学', ['small-story/index', 'category' => 'jpm'], ['class' => 'nav-link']);
+            //            echo Html::a('看电影', 'https://www.52film.top', ['class' => 'nav-link', 'target' => '_blank']);
             echo Html::a('历史', ['small-story/index', 'category' => 'china_history,world_history,hougong'], ['class' => 'nav-link']);
             echo Html::a('爱情', ['small-story/index', 'category' => 'city,classic,chulian,love_school,wangluoaiqing'], ['class' => 'nav-link']);
             echo Html::a('哲理', ['small-story/index', 'category' => 'chanli,yuyan,zhihui'], ['class' => 'nav-link']);
@@ -90,7 +91,8 @@ AppAsset::register($this);
         <div class="hidden-sm hidden-md hidden-lg col-xs-12  center-block">
             <form class="navbar-form navbar-left form-inline" role="" action="/">
                 <div class="form-group col-xs-9">
-                    <input type="text" class="form-control" name="SmallStorySearch[content]" placeholder="关键词" value="<?=$keyword?>">
+                    <input type="text" class="form-control" name="SmallStorySearch[content]" placeholder="关键词"
+                           value="<?= $keyword ?>">
                 </div>
                 <div class="col-xs-2">
                     <button type="submit" class="btn btn-default">搜索</button>
@@ -114,9 +116,10 @@ AppAsset::register($this);
 </footer>
 
 <?php $this->endBody() ?>
-<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101162154);</script>
+<script>var clicky_site_ids = clicky_site_ids || [];
+    clicky_site_ids.push(101162154);</script>
 <script async src="//static.getclicky.com/js"></script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101162154ns.gif" /></p></noscript>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101162154ns.gif"/></p></noscript>
 </body>
 </html>
 <?php $this->endPage() ?>
